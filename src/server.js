ed15c6b381b9b168be888731d60b965be02547f1
higebobo <higebobo@gmail.com>
2021-11-11 10:20:04 +0900

    modified

--- src/server.js
+++ src/server.js
@@ -17,11 +17,11 @@ router.post('/webhook', line.middleware(config), (req, res) => {
     console.log(req.body.events);
 
     //ここのif分はdeveloper consoleの"接続確認"用なので削除して問題ないです。
-    if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
-        res.send('Hello LINE BOT!(POST)');
-        console.log('疎通確認用');
-        return;
-    }
+    //if(req.body.events[0].replyToken === '00000000000000000000000000000000' && req.body.events[1].replyToken === 'ffffffffffffffffffffffffffffffff'){
+    //    res.send('Hello LINE BOT!(POST)');
+    //    console.log('疎通確認用');
+    //    return;
+    //}
 
     Promise
       .all(req.body.events.map(handleEvent))
